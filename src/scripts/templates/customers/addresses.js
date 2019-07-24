6eb7031d23aced760ffb11d2e5d58e189376b88f
AxeemHaider <axeemhaider@gmail.com>
2019-07-24 16:08:49 +0500

    Customer address complete

--- src/scripts/templates/customers/addresses.js
+++ src/scripts/templates/customers/addresses.js
@@ -9,29 +9,30 @@
 
 import {AddressForm} from '@shopify/theme-addresses';
 
+
 const selectors = {
   addressContainer: '[data-address]',
   addressFields: '[data-address-fields]',
   addressToggle: '[data-address-toggle]',
   addressForm: '[data-address-form]',
   addressDeleteForm: '[data-address-delete-form]',
+  addressHeading: '[data-address-heading-element]',
+  addNewAddress : '[data-s-add-new-address]',
+  editAddress: '[data-s-edit-address]',
+  cancelAddress: '[data-s-cancel-address]',
+  cancelNewAddress: '[data-s-cancel-new-address]',
+  customerAdrs: '[data-s-customer-address]'
 };
-const hideClass = 'hide';
+const hideClass = 'd-none';
 
 function initializeAddressForm(container) {
   const addressFields = container.querySelector(selectors.addressFields);
-  const addressForm = container.querySelector(selectors.addressForm);
   const deleteForm = container.querySelector(selectors.addressDeleteForm);
 
-  container.querySelectorAll(selectors.addressToggle).forEach((button) => {
-    button.addEventListener('click', () => {
-      addressForm.classList.toggle(hideClass);
-    });
-  });
 
   AddressForm(addressFields, 'en');
 
-  if (deleteForm) {
+  /* if (deleteForm) {
     deleteForm.addEventListener('submit', (event) => {
       const confirmMessage = deleteForm.getAttribute('data-confirm-message');
 
@@ -39,13 +40,113 @@ function initializeAddressForm(container) {
         event.preventDefault();
       }
     });
-  }
+  } */
 }
 
 const addressForms = document.querySelectorAll(selectors.addressContainer);
-
 if (addressForms.length) {
   addressForms.forEach((addressContainer) => {
     initializeAddressForm(addressContainer);
   });
+}
+
+const addNewAdrs = document.querySelector(selectors.addNewAddress);
+addNewAdrs.addEventListener('click', function(){
+  addNewAddress();
+});
+
+const cancelNewAdrs = document.querySelector(selectors.cancelNewAddress);
+cancelNewAdrs.addEventListener('click', (event) => {
+  cancelNewAddress(event);
+});
+
+const editAdrs = document.querySelectorAll(selectors.editAddress);
+if(editAdrs.length) {
+  editAdrs.forEach((addr) => {
+    const id = addr.dataset.addressId;
+    addr.addEventListener('click', () => {
+      editAddress(id);
+    });
+  });
+}
+
+const cancelAdrs = document.querySelectorAll(selectors.cancelAddress);
+if(cancelAdrs.length) {
+  cancelAdrs.forEach((addr) => {
+    const id = addr.dataset.addressId;
+    addr.addEventListener('click', (event) => {
+      cancelEditAddress(event, id);
+    });
+  });
+}
+
+function editAddress(addressId) {
+  var addressHeading = document.getElementById('address-heading');
+  addressHeading.innerText = "EDIT ADDRESS";
+
+  var coustomerAdrss = document.querySelectorAll(selectors.customerAdrs);
+  if(coustomerAdrss != null) {
+    coustomerAdrss.forEach((adr) => {
+      adr.style.display = "none";
+    });
+  }
+
+  var editAddress = document.getElementById("edit-address-" + addressId);
+  editAddress.style.display = "block";
+
+  var addNewAddress = document.getElementById("add-new-address");
+  addNewAddress.style.display = "none";
+}
+
+function cancelEditAddress(event, addressId) {
+  event.preventDefault();
+
+  var addressHeading = document.getElementById('address-heading');
+  addressHeading.innerText = "YOUR ADDRESSES";
+
+  var coustomerAdrss = document.querySelectorAll(selectors.customerAdrs);
+  if (coustomerAdrss != null) {
+    coustomerAdrss.forEach((adr) => {
+      adr.style.display = "block";
+    });
+  }
+
+  var editAddress = document.getElementById("edit-address-" + addressId);
+  editAddress.style.display = "none";
+
+  var addNewAddress = document.getElementById("add-new-address");
+  addNewAddress.style.display = "none";
+}
+
+function addNewAddress() {
+
+  var addressHeading = document.getElementById('address-heading');
+  addressHeading.innerText = "NEW ADDRESS";
+
+  var coustomerAdrss = document.querySelectorAll(selectors.customerAdrs);
+  if (coustomerAdrss != null) {
+    coustomerAdrss.forEach((adr) => {
+      adr.style.display = "none";
+    });
+  }
+
+  var addNewAddress = document.getElementById("add-new-address");
+  addNewAddress.style.display = "block";
+}
+
+function cancelNewAddress(event) {
+  event.preventDefault();
+
+  var addressHeading = document.getElementById('address-heading');
+  addressHeading.innerText = "YOUR ADDRESSES";
+
+  var coustomerAdrss = document.querySelectorAll(selectors.customerAdrs);
+  if(coustomerAdrss != null) {
+    coustomerAdrss.forEach((adr) => {
+      adr.style.display = "block";
+    });
+  }
+
+  var addNewAddress = document.getElementById("add-new-address");
+  addNewAddress.style.display = "none";
 }
