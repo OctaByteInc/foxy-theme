e9069268283cd7dbeca5ed4bf9fe16c3892622b1
AxeemHaider <axeemhaider@gmail.com>
2019-08-17 15:50:54 +0500

    Wishlist app complete

--- src/assets/static/addon/js/addon-wishlist.js
+++ src/assets/static/addon/js/addon-wishlist.js
@@ -1,7 +1,8 @@
 
 var wishlistProduct = {
     fetch: 'addon-wishlist',
-    render: 'addon-wishlist-render'
+    render: 'addon-wishlist-render',
+    btn: 'addon-wishlist-btn'
 };
 
 var wishlistSettings = {
@@ -11,11 +12,17 @@ var wishlistSettings = {
 function bindWishlist() {
 
     var wishlistBtns = document.getElementsByClassName(wishlistProduct.fetch);
-
+    
     for (btn of wishlistBtns) {
 
-        btn.addEventlistener('click', function(){
-            saveWishlistItem(btn);
+        btn.addEventListener('click', function(){
+            this.classList.replace('ion-ios-heart-outline', 'ion-android-done-all');
+            
+            if (!this.dataset.addedInWishlist) {
+                this.dataset.addedInWishlist = true;
+                saveWishlistItem(this);                
+            }
+
         });
 
     }
@@ -24,6 +31,7 @@ function bindWishlist() {
 
 function saveWishlistItem(product){
 
+    var uuid = new Date().getTime();
     var handle = product.dataset.productHandle;
     var image = product.dataset.productImage;
     var title = product.dataset.productTitle;
@@ -35,7 +43,7 @@ function saveWishlistItem(product){
         var wishListStorage = localStorage.getItem(wishlistSettings.storage);
         var wishlist = JSON.parse(wishListStorage);
 
-        wishlist.push({ handle, image, title, salePrice, regularPrice });
+        wishlist.push({ uuid, handle, image, title, salePrice, regularPrice });
 
         wishListStorage = JSON.stringify(wishlist);
 
@@ -52,7 +60,7 @@ function saveWishlistItem(product){
 
 }
 
-function deleteWishlistItem(handle) {
+function deleteWishlistItem(uuid) {
 
     if ( localStorage.getItem(wishlistSettings.storage) != null ) {
 
@@ -61,10 +69,12 @@ function deleteWishlistItem(handle) {
 
         var newWishlist = wishlist.filter(function(product){
 
-            if (product.handle == handle) {
+            if (product.uuid == uuid) {
+                var productElement = document.getElementById('wishlist-'+uuid);
+                productElement.style.display = 'none';
                 return false;
             } else {
-                true;
+                return true;
             }
 
         });
@@ -97,19 +107,32 @@ function renderWishlistProduct() {
 }
 
 function singleWishlistProduct(product) {
-    var html  = '<li class="single-product-cart">';
+    var html  = '<li id="wishlist-'+product.uuid+'" class="single-product-cart">';
         html +=     '<div class="cart-img">';
         html +=         '<a href="/products/'+product.handle+'"><img src="'+product.image+'"></a>';
         html +=     '</div>';
         html +=     '<div class="cart-title">';
         html +=         '<h3><a href="/products/'+product.handle+'">'+product.title+'</a></h3>';
         html +=         '<span>'+product.salePrice+'</span>';
-        html +=         '<span>'+product.regularPrice+'</span>';
+        html +=         '<del><span style="margin-left: 8px;color: #333;">'+product.regularPrice+'</span></del>';
         html +=     '</div>';
         html +=     '<div class="cart-delete">';
-        html +=         '<a href="javascript:void(0)" onclick="deleteWishlistItem('+product.handle+')"><i class="ion-ios-trash-outline"></i></a>';
+        html +=         '<a href="javascript:void(0)" onclick="deleteWishlistItem('+product.uuid+')"><i class="ion-ios-trash-outline"></i></a>';
         html +=     '</div>';
         html += '</li>';
 
     return html;
+}
+
+// Initalize Addon Wish List
+window.onload = function(){
+
+    bindWishlist();
+
+    // Bind wishlist header button for wishlist product rendering
+    var wishListBtn = document.getElementById(wishlistProduct.btn);
+    wishListBtn.addEventListener('click', function(){
+        renderWishlistProduct();
+    });
+
 }
